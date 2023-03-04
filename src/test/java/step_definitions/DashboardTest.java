4cec195e02456e2615a7376774d33eae8c1f265f
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-03-04 23:22:50 +0300

    second push

--- src/test/java/step_definitions/DashboardTest.java
+++ src/test/java/step_definitions/DashboardTest.java
@@ -4,11 +4,14 @@ import io.cucumber.java.en.*;
 import org.junit.*;
 import org.openqa.selenium.*;
 import pages.*;
+import utilities.*;
 
 import java.util.*;
 
 public class DashboardTest {
 
+
+    VyTrackLoginPage loginPage = new VyTrackLoginPage();
     DashboardPage dashboardPage = new DashboardPage();
 
     @Then("user should see menu options")
@@ -18,8 +21,34 @@ public class DashboardTest {
         for (WebElement menu : dashboardPage.options) {
             actualOptions.add(menu.getText());
         }
-        Assert.assertEquals(expectedOptions,actualOptions);
+        Assert.assertEquals(expectedOptions, actualOptions);
 
+    }
+
+    @When("user enters as different {string}")
+    public void user_enters_as_different(String role) {
+        String username = "";
+        String password = "";
+        switch (role) {
+            case "user":
+                username = ConfigurationReader.getProperty("driver_username");
+                password = ConfigurationReader.getProperty("driver_password");
+                break;
+
+            case "sales manager":
+                username = ConfigurationReader.getProperty("sales_manager_username");
+                password = ConfigurationReader.getProperty("sales_manager_password");
+                break;
+
+            case "store manager":
+                username = ConfigurationReader.getProperty("store_manager_username");
+                password = ConfigurationReader.getProperty("store_manager_password");
+                break;
+        }
+
+        loginPage.userNameInput.sendKeys(username);
+        loginPage.passwordInput.sendKeys(password);
+        loginPage.submitBtn.click();
     }
 
 }
--- src/test/resources/features/dashboardTest.feature
+++ src/test/resources/features/dashboardTest.feature
@@ -1,14 +1,50 @@
 Feature: Dashboard Test
 
-  @wip
-  Scenario: Check dashboard elements
+  Background:
     Given user is on the login page
-    When user enters "user1" and "UserUser123" as credentials
+
+  @wip
+  Scenario Outline: Check dashboard elements as user
+    When user enters as different "<role>"
     Then user should see menu options
-      | Fleet      |
-      | Customers  |
-      | Activities |
-      | System     |
+      | Dashboard       |
+      | Fleet           |
+      | Customers       |
+      | Sales           |
+      | Activities      |
+      | Marketing       |
+      | Report&Segments |
+      | System          |
+
+    Examples:
+      | role          |
+      | sales manager |
+      | store manager |
+
+#  @smoke
+#  Scenario: Check dashboard elements as sales manager
+#    When user enters "salesmanager101" and "UserUser123" as credentials
+#    Then user should see menu options
+#      | Dashboard       |
+#      | Fleet           |
+#      | Customers       |
+#      | Sales           |
+#      | Activities      |
+#      | Marketing       |
+#      | Report&Segments |
+#      | System          |
+#
+#  Scenario: Check dashboard elements as store manager
+#    When user enters "storemanager85" and "UserUser123" as credentials
+#    Then user should see menu options
+#      | Dashboard       |
+#      | Fleet           |
+#      | Customers       |
+#      | Sales           |
+#      | Activities      |
+#      | Marketing       |
+#      | Report&Segments |
+#      | System          |
 
 
 
